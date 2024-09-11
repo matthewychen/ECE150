df56f52fda6f31bb756cf1f5ccce31935541a9ca
matthewychen <matthewychen06@gmail.com>
2024-09-11 08:45:13 -0400

    added Lab_2

---
+++ Lab_2/gooseParty/gooseB.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+  std::cout << "   ";
+  std::cout << "  p<";
+  std::cout << std::endl;
+  std::cout << "\"_";
+  std::cout << "_/";
+  std::cout << std::endl;
+  std::cout << " \\=";
+  std::cout << "/";
+  std::cout << std::endl;
+  std::cout << "  ";
+  std::cout << "\\_";
+  std::cout << std::endl;
+  return 0;
+}
---
+++ Lab_2/gooseParty/gooseF.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+// \b is backspace or back-delete
+// \t is tab
+// \f is a curse on all who use it
+int main() {
+  std::cout << "     p<\n";
+  std::cout << "\"___/\b";
+  std::cout << " \\=/\f";
+  std::cout << "  \\_\t";
+  return 0;
+}
