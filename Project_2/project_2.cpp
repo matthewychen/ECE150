70dfd7e9be4ad027dd629b7e345f07484bc78b54
matthewychen <matthewychen06@gmail.com>
2024-09-26 11:04:23 -0400

    a

---
+++ Project_2/project_2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+
+unsigned int n{};
+
+void pattern( unsigned int n ){
+    while(true){
+        
+            for(unsigned int b;b<=n;++b){
+                std::cout<<'*'*b<<;
+    }
+    }
+}
+
+int log10(unsigned int n){
+    int counter{};
+    int counter=0;
+    while(true){
+        if(10^counter<n){
+            counter +=1;
+        }
+        else{
+            return counter;
+        }
+    }
+}
+
+int count(unsigned int n, unsigned int bit){
+    
+    }
+
+int swap_bytes( unsigned int n, unsigned int b0, unsigned int b1 ){
+    
+}
