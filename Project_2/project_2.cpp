a07935d5c6e3167b76891eb266e2cbd6838b88e2
Matthew Chen <matthewychen06@gmail.com>
2024-10-09 03:00:34 -0400

    finished proj2

--- Project_2/project_2.cpp
+++ Project_2/project_2.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 #include <cassert>
 
 unsigned int n{};
@@ -8,7 +7,7 @@ void pattern( unsigned int n ){
     unsigned int asterisks{2*n+1};
     unsigned int spaces{0};
     bool hitmid{false};
-        for(int line = 0;line>=0 && line<=n;){
+        for(unsigned int line = 0;line>=0 && line<=n;){
             //printing out
             for(unsigned int i = 0; i<spaces ;i++){
                 std::cout<<' ';
@@ -19,7 +18,7 @@ void pattern( unsigned int n ){
             for(unsigned int i=0 ; i<spaces ; i++){
                 std::cout<<' ';
             }
-            std::cout<<'\n';
+            std::cout<<std::endl;
 
             //increment upwards if not yet at middle
             if(hitmid==false){
@@ -43,35 +42,47 @@ void pattern( unsigned int n ){
 unsigned int log10(unsigned int n){ 
     assert(n!=0);
     unsigned int counter{0};
-    unsigned int power{1};
-    if(n>=1){
-    while(power<=n){
-        power*=10;
-        counter+=1;
+    while(n>=1){
+        n/=10;
+        counter++;
         }
-    }
     return (counter-1);
 }
 
-unsigned int count(unsigned int n, unsigned int bit){
-    assert(bit==0 || bit==1);
-    unsigned int numbits{0};
+unsigned int count(unsigned int n, unsigned int bit) {
+    assert(bit == 0 || bit == 1);
     unsigned int count{0};
-    while (n!=0){
-        if(n%2==bit){
-        count+=1;
+    unsigned int alrprocessed = 0;
+    while (n != 0) {
+        if ((n & 1) == bit) {
+            count += 1;
         }
-        n/=2;
+        n >>= 1;
+        alrprocessed++;
+    }
+    if (bit == 0 && alrprocessed < 32) {
+        count += (32 - alrprocessed);
     }
     return count;
-    }
+}
+
 
 unsigned int swap_bytes( unsigned int n, unsigned int b0, unsigned int b1 ){
-    return 0;
+    assert((b0>=0&&b0<=3)&&(b1>=0&&b1<=3));
+    unsigned int numb0{};
+    unsigned int numb1{};
+    numb0 = n>>(8*b0) & 0xFF; //isolate for byte by shift original bit to the right and taking the union of that with the last 2 bytes
+    numb1 = n>>(8*b1) & 0xFF;
+
+    n &= ~(0xFF<<8*b0);//clear position b1 and b0 while maintaining everywhere else
+    n &= ~(0xFF<<8*b1);
+
+    n |=(numb0<<8*b1);//sub new values in at new position
+    n |=(numb1<<8*b0);
+    return n;
 }
 
 int main(){
-    count(20,0);
-    log10(20);
+        std::cout<<log10(10);
     return 0;
 }
